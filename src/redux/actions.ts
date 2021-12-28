f0881c2f08e20c8e3feeac8418a4caa9159f0f6a
vladward <vladward13@gmail.com>
2021-12-28 11:58:27 +0300

    create actions and actions creators

---
+++ src/redux/actions.ts
@@ -0,0 +1,53 @@
+export enum ACTIONS_TYPE {
+    CHANGE_CURRENCY_FIELD_TYPE = 'CurrencyExchange/CHANGE_CURRENCY_FIELD_TYPE',
+    CHANGE_CHANGE_ACTION = 'CurrencyExchange/CHANGE_CHANGE_ACTION',
+    CHANGE_CURRENT_CURRENCY = 'CurrencyExchange/CHANGE_CURRENT_CURRENCY',
+}
+
+
+export type ChangeCurrencyFieldType = {
+    type: ACTIONS_TYPE.CHANGE_CURRENCY_FIELD_TYPE,
+    payload: {
+        amountOfBYN: string,
+        amountOfCurrency: string,
+    },
+};
+
+export const ChangeCurrencyFieldAC = (amountOfBYN: string, amountOfCurrency: string): ChangeCurrencyFieldType => {
+    return {type: ACTIONS_TYPE.CHANGE_CURRENCY_FIELD_TYPE, payload: {amountOfBYN, amountOfCurrency}}
+};
+
+export type ChangeAction = {
+    type: ACTIONS_TYPE.CHANGE_CHANGE_ACTION,
+    payload: {
+        isBuying: boolean,
+        amountOfBYN: '',
+        amountOfCurrency: '',
+        // зануляю значения инпута что бы не было ошибки конвертации при выборе sell / buy
+    }
+};
+
+export const ChangeActionAC = (isBuying: boolean): ChangeAction => {
+    return {
+        type: ACTIONS_TYPE.CHANGE_CHANGE_ACTION,
+        payload: {isBuying, amountOfBYN: '', amountOfCurrency: ''}
+    }
+};
+
+export type ChangeCurrentCurrencyType = {
+    type: ACTIONS_TYPE.CHANGE_CURRENT_CURRENCY,
+    payload: {
+        currentCurrency: string,
+        amountOfBYN: '',
+        amountOfCurrency: '',
+    }
+};
+
+export const ChangeCurrentCurrencyAC = (currentCurrency: string): ChangeCurrentCurrencyType => {
+    return {
+        type: ACTIONS_TYPE.CHANGE_CURRENT_CURRENCY,
+        payload: {currentCurrency, amountOfBYN: '', amountOfCurrency: '',}
+    }
+};
+
+export type CurrencyReducersTypes = ChangeCurrencyFieldType | ChangeAction | ChangeCurrentCurrencyType;

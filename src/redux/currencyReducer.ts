92632aecd323c7aeadbb315e0ce1a795bef20892
vladward <vladward13@gmail.com>
2021-12-28 11:58:56 +0300

    create currencyReducer, types, initial state

---
+++ src/redux/currencyReducer.ts
@@ -0,0 +1,52 @@
+import {ACTIONS_TYPE, CurrencyReducersTypes} from './actions';
+
+export type CurrencyType = {
+    currencyName: string;
+    buyRate: number;
+    sellRate: number;
+};
+export type CurrencyState = {
+    currencies: Array<CurrencyType>;
+    currentCurrency: string;
+    isBuying: boolean;
+    amountOfBYN: string;
+    amountOfCurrency: string;
+};
+
+const initialState: CurrencyState = {
+    currencies: [
+        {
+            currencyName: 'USD',
+            buyRate: 2.58,
+            sellRate: 2.49,
+        },
+        {
+            currencyName: 'EUR',
+            buyRate: 2.9,
+            sellRate: 2.87,
+        },
+        {
+            currencyName: 'RUR',
+            buyRate: 0.0345,
+            sellRate: 0.0341,
+        },
+    ],
+    currentCurrency: 'USD',
+    isBuying: true,
+    amountOfBYN: '',
+    amountOfCurrency: '',
+}
+
+export const currencyReducer = (state: CurrencyState = initialState, action: CurrencyReducersTypes): CurrencyState => {
+    switch (action.type) {
+        case ACTIONS_TYPE.CHANGE_CURRENCY_FIELD_TYPE:
+        case ACTIONS_TYPE.CHANGE_CHANGE_ACTION:
+        case ACTIONS_TYPE.CHANGE_CURRENT_CURRENCY:
+            return {
+                ...state,
+                ...action.payload
+            }
+        default:
+            return state;
+    }
+};
